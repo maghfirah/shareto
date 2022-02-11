db96ae2c95bd77435007f58bb0c74591ab43a3eb
Maghfirah suyuti <maghfirah.suyuti@gmail.com>
2022-02-11 19:00:36 +0800

    update env

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,9 +2,11 @@
     window["env"] = window["env"] || {};
 
     // Environment variables
-    window["env"]["apiUrl"] = "http://localhost:8080/retail-api/v1";
-    window["env"]["envUrl"] = "https://app-uat.doku.com";
+    // window["env"]["apiUrl"] = "http://localhost:8080/retail-api/v1";
+    window["env"]["apiUrl"] = "https://app-uat.doku.com/retail-api/v1";
+    // window["env"]["envUrl"] = "https://app-uat.doku.com";
     // window["env"]["envUrl"] = "http://localhost:4200";
+    window["env"]["envUrl"] = "https://shareto2.vercel.app";
     window["env"]["gtmId"] = "";
     window["env"]["assetsUrl"] = "https://cdn-dev.oss-ap-southeast-5.aliyuncs.com";
 })(this);
